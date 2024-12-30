3ee42ab922190e188162aefaac7c6b4009adb98a
Piers Sinclair <piers2307@gmail.com>
2024-12-30 09:39:07 +0800

    Add giscus

--- pierssinclair-blog/src/pages/post/Post.tsx
+++ pierssinclair-blog/src/pages/post/Post.tsx
@@ -3,17 +3,18 @@ import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
 import { fetchPost, PostModel } from "../../utils/postUtils";
 import { format } from "date-fns";
+import Giscus from '@giscus/react';
 
 const BlogPost: React.FC = () => {
     const { slug } = useParams<{ slug: string }>();
     const [post, setPost] = useState<PostModel | undefined>();
 
     useEffect(() => {
-        if(!slug)
+        if (!slug)
             return;
 
         console.log(slug);
-        const loadPost = async (fileName: string) => { 
+        const loadPost = async (fileName: string) => {
             const post = await fetchPost(fileName);
             setPost(post);
         };
@@ -21,16 +22,30 @@ const BlogPost: React.FC = () => {
         loadPost(slug);
     }, [slug]);
 
-    if(!post) return <div>Loading...</div>;
+    if (!post) return <div>Loading...</div>;
     return (
         <div className="container mx-auto py-12 max-w-screen-md">
             <h1 className="text-5xl font-bold mb-4 text-gray-100">{post.frontmatter.title}</h1>
             <p className="text-gray-400 mb-4">{format(new Date(post.frontmatter.date), 'dd MMM, yyyy')} · {post.frontmatter.author}</p>
             <hr className="border-gray-700 mb-8" />
-  
+
             <div className="prose prose-lg prose-invert max-w-none text-sm">
                 <ReactMarkdown>{post.content}</ReactMarkdown>
             </div>
+            <Giscus
+                repo="piers-sinclair/pierssinclairssw.github.io"
+                repoId="MDEwOlJlcG9zaXRvcnkzODQ5NDU4NDk="
+                category="General"
+                categoryId="DIC_kwDOFvHOuc4Cf9Sl"
+                mapping="pathname"
+                strict="0"
+                reactionsEnabled="1"
+                emitMetadata="0"
+                inputPosition="bottom"
+                theme="dark"
+                lang="en"
+                loading="lazy"
+            />
         </div>
     );
 };
