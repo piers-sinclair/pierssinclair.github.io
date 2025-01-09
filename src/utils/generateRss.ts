43a17eae3edda9e48ff873477c5cf362375e02a4
Piers Sinclair <piers2307@gmail.com>
2025-01-09 22:42:13 +1100

    Migrate to Vite

--- src/utils/generateRss.ts
+++ src/utils/generateRss.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
+import fs from "fs";
+import path from "path";
 import { marked } from "marked";
 import { Feed } from "feed";
 import { fetchPostMetadata } from "./postUtils";
